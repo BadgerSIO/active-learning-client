3ef97e1bfd480d7895e6d8cf2a5f066858a919d1
BadgerSIO <saadibneomar@gmail.com>
2022-10-25 00:25:43 +0600

    router setup done and logo added

---
+++ src/Header/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+import logo from "../logo.svg";
+const Header = () => {
+  return (
+    <div className="bg-slate-800">
+      <img src={logo} alt="" className="h-14" />
+    </div>
+  );
+};
+
+export default Header;
